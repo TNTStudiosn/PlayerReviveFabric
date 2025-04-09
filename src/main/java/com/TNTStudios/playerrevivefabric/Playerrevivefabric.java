929e832fb9dbd24ff7df8a289eeb96c7d4c3f3a7
TNTStudios <tntstudiosn@gmail.com>
2025-04-09 04:28:47 -0600

    1

--- src/main/java/com/TNTStudios/playerrevivefabric/Playerrevivefabric.java
+++ src/main/java/com/TNTStudios/playerrevivefabric/Playerrevivefabric.java
@@ -8,9 +8,12 @@ import com.TNTStudios.playerrevivefabric.server.PlayerReviveHandler;
 import com.TNTStudios.playerrevivefabric.data.ReviveManager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 
 public class Playerrevivefabric implements ModInitializer {
 
@@ -27,6 +30,15 @@ public class Playerrevivefabric implements ModInitializer {
             }
             PlayerReviveHandler.tick(server);
         });
+
+        ServerLivingEntityEvents.ALLOW_DAMAGE.register((entity, source, amount) -> {
+            if (entity instanceof ServerPlayerEntity player) {
+                return DeathEventHandler.onPlayerDamaged(player, source, amount) != ActionResult.FAIL;
+            }
+            return true; // Permitir daño por defecto
+        });
+
+
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             AcceptDeathCommand.register(dispatcher);
